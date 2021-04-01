8a2a50b489b3db39afa410534674e777120e3977
JWalters <joshwalters34@gmail.com>
2021-03-31 22:02:43 -0400

    fixed prompt functions for each team member.  Added functions to call different data to lib files.  Created code to write the html file to output folder.

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const Temp = require('./src/page-template')
+const generateTeam = require('./src/page-template');
+const Manager = require('./lib/Manager.js');
+const Engineer = require('./lib/Engineer.js');
+const Intern = require('./lib/Intern.js');
+const Employee = require('./lib/Employee');
+const teamMembers = [];
+const path = require('path');
+const OUTPUT_DIR = path.resolve(__dirname, "output")
+const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-// const temp = new Temp();
 
-function init() {
-    inquirer
+
+function manager() {
+  inquirer
     .prompt([
       {
         type: 'input',
@@ -26,93 +34,132 @@ function init() {
         // validate: validatePrompts,
       },
       {
-          type: 'input',
-          message: 'What is the team managers office number?',
-          name: 'managerOffice',
+        type: 'input',
+        message: 'What is the team managers office number?',
+        name: 'managerOffice',
         //   validate: validatePrompts,
       },
-      {
-        type: 'list',
-        message: 'Would you like to enter a new team member?',
-        name: 'newMember',
-        choices: ['Engineer', 'Intern', 'Done'],
-    },
-    {
-        type: 'input',
-        message: 'What is the engineers name?',
-        name: 'engineerName',
-      //   validate: validatePrompts,
-        when: (answers) => answers.newMember === 'Engineer'
-    },
-    {
-        type: 'input',
-        message: 'What is the engineers ID?',
-        name: 'engineerID',
-      //   validate: validatePrompts,
-        when: (answers) => answers.newMember === 'Engineer'
-    },
-    {
-        type: 'input',
-        message: 'What is the engineers email address?',
-        name: 'engineerEmail',
-      //   validate: validatePrompts,
-        when: (answers) => answers.newMember === 'Engineer'
-    },
-    {
-        type: 'input',
-        message: 'What is the engineers GitHub username',
-        name: 'engineerGit',
-      //   validate: validatePrompts,
-        when: (answers) => answers.newMember === 'Engineer'
-    },
-    {
-        type: 'list',
-        message: 'Would you like to enter a new team member?',
-        name: 'newMember',
-        choices: ['Engineer', 'Intern', 'Done'],
-        when: (answers) => answers.newMember === 'Engineer'
-    },
-        {
-        type: 'input',
-        message: 'What is the interns name?',
-        name: 'internName',
-      //   validate: validatePrompts,
-        when: (answers) => answers.newMember === 'Intern'
-    },
-    {
-        type: 'input',
-        message: 'What is the interns ID?',
-        name: 'internID',
-      //   validate: validatePrompts,
-        when: (answers) => answers.newMember === 'Intern'
-    },
-    {
-        type: 'input',
-        message: 'What is the interns email address?',
-        name: 'internEmail',
-      //   validate: validatePrompts,
-        when: (answers) => answers.newMember === 'Intern'
-    },
-    {
-        type: 'input',
-        message: 'What school does the intern attend?',
-        name: 'internSchool',
-      //   validate: validatePrompts,
-        when: (answers) => answers.newMember === 'Intern'
-    },
-    {
-        type: 'list',
-        message: 'Would you like to enter a new team member?',
-        name: 'newMember',
-        choices: ['Engineer', 'Intern', 'Done'],
-    },
-    ])
-    .then (response => {
-      fs.writeFile('index.html', Temp.generateTeam(response)
-      , (err) =>
-      err ? console.log(err) : console.log("HTML generated")
-      )
-    })
-  }
 
-  init();
+    ])
+    .then(response => {
+      const manager = new Manager(response.managerName, response.managerID, response.managerEmail, response.managerOffice);
+      teamMembers.push(manager);
+      newMemberPrompt()
+        // , (err) =>
+        //   err ? console.log(err) : console.log("HTML generated")
+    })
+}
+
+function newMemberPrompt() {
+
+  inquirer.prompt([
+    {
+      type: 'list',
+      message: 'Would you like to enter a new team member?',
+      name: 'newMember',
+      choices: ['Engineer', 'Intern', 'Done'],
+    },
+  ])
+    .then((response) => {
+      // const employee = new Employee(response.newMember);
+      // teamMembers.push(employee);
+      switch (response.newMember) {
+        case 'Engineer':
+          engineerPrompt();
+          break;
+
+        case 'Intern':
+          internPrompt();
+          break;
+
+        default:
+          fs.writeFileSync(outputPath, generateTeam(teamMembers))
+          // break;
+      }
+      // , (err) =>
+      // err ? console.log(err) : console.log("HTML generated")
+    })
+}
+
+function engineerPrompt() {
+  inquirer.prompt([
+    {
+      type: 'input',
+      message: 'What is the engineers name?',
+      name: 'engineerName',
+      //   validate: validatePrompts,
+      // when: (answers) => answers.newMember === 'Engineer'
+    },
+    {
+      type: 'input',
+      message: 'What is the engineers ID?',
+      name: 'engineerID',
+      //   validate: validatePrompts,
+      // when: (answers) => answers.newMember === 'Engineer'
+    },
+    {
+      type: 'input',
+      message: 'What is the engineers email address?',
+      name: 'engineerEmail',
+      //   validate: validatePrompts,
+      // when: (answers) => answers.newMember === 'Engineer'
+    },
+    {
+      type: 'input',
+      message: 'What is the engineers GitHub username',
+      name: 'engineerGit',
+      //   validate: validatePrompts,
+      // when: (answers) => answers.newMember === 'Engineer'
+    },
+
+  ])
+    .then(response => {
+      const engineer = new Engineer(response.engineerName, response.engineerID, response.engineerEmail, response.engineerGit);
+      teamMembers.push(engineer);
+      newMemberPrompt()
+        // , (err) =>
+        //   err ? console.log(err) : console.log("HTML generated")
+    })
+}
+
+function internPrompt() {
+  inquirer.prompt([
+    {
+      type: 'input',
+      message: 'What is the interns name?',
+      name: 'internName',
+      //   validate: validatePrompts,
+      // when: (answers) => answers.newMember === 'Intern'
+    },
+    {
+      type: 'input',
+      message: 'What is the interns ID?',
+      name: 'internID',
+      //   validate: validatePrompts,
+      // when: (answers) => answers.newMember === 'Intern'
+    },
+    {
+      type: 'input',
+      message: 'What is the interns email address?',
+      name: 'internEmail',
+      //   validate: validatePrompts,
+      // when: (answers) => answers.newMember === 'Intern'
+    },
+    {
+      type: 'input',
+      message: 'What school does the intern attend?',
+      name: 'internSchool',
+      //   validate: validatePrompts,
+      // when: (answers) => answers.newMember === 'Intern'
+    },
+  ])
+    .then(response => {
+      const intern = new Intern(response.internName, response.internID, response.internEmail, response.internSchool);
+      teamMembers.push(intern);
+      newMemberPrompt()
+        // , (err) =>
+        //   err ? console.log(err) : console.log("HTML generated")
+    });
+}
+
+manager();
